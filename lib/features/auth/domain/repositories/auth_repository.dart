51fe9c1821da55482a410166ad1f243bf0146e56
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-07-08 22:55:24 +0700

    feat : membuat abstark kelas yg berfungsi untuk mennetukan fungsi apasaja yang harus tersedia login& logout contohnya

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:armada/core/error/failure.dart';
+import 'package:armada/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepository {
+  // Kontrak untuk fungsi Login
+  Future<Either<Failure, UserEntity>> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  // Kontrak untuk fungsi Logout
+  Future<Either<Failure, void>> logout();
+}
